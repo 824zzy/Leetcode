e268602ceb6a436fa6ea69ad07cbc9519340d41e
824zzy <614594368@qq.com>
2019-06-21 14:48:50 +0800

    docs($location): today's leet

--- 5_Longest_Palindromic_Substring_L1.py
+++ 5_Longest_Palindromic_Substring_L1.py
@@ -1,10 +1,39 @@
-"""
+""" Sliding windows with advanced palindrome-judgement
+3680ms, faster than vanilla palindrome-judgement
 """
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+        window = len(s)
 
+        while 1:
+            for i in range(len(s)+1-window):
+                subs = s[i:window+i]
+                if subs == subs[::-1]:
+                    return subs
+            window -= 1
 
+""" Sliding windows with vanilla palindrome-judgement
+8556ms, slow enough.
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+        window = len(s)
 
+        while 1:
+            for i in range(len(s)+1-window):
+                subs = s[i:window+i]
+                is_palin = True
+                for i in range(len(subs)):
+                    if subs[i] != subs[-1-i]:
+                        is_palin = False
+                        break
+                if is_palin:
+                    return subs   
+            window -= 1
 
 """ Note that brute force not work O(n^3)
 """
---
+++ 832.flipping-an-image.py
@@ -0,0 +1,9 @@
+#
+# @lc app=leetcode id=832 lang=python3
+#
+# [832] Flipping an Image
+#
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        
+
