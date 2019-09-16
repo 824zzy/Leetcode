d3eaa328b67d4f1a4235f0bdc6d18324b2629cea
824zzy <614594368@qq.com>
2019-09-16 18:46:01 -0500

    docs($location): 9.16.19

---
+++ 2_String/824_Goat_Latin_L0.py
@@ -0,0 +1,13 @@
+""" Naive problem. Not worth to solve it.
+"""
+class Solution:
+    def toGoatLatin(self, S: str) -> str:
+        vowel = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        ans = ''
+        
+        for i, e in enumerate(S.split()):
+            if e[0] in vowel:
+                ans += e+'ma'+'a'*(i+1)+' '
+            else:
+                ans += e[1:]+e[0]+'ma'+'a'*(i+1)+' '
+        return ans[:-1]
