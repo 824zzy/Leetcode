de54c43eed1c41369de52e7eda87e8811f5fd538
824zzy <614594368@qq.com>
2019-09-30 19:17:20 -0500

    docs($location): 19 9 30

---
+++ 2_String/345_Reverse_Vowels_of_a_String_L0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        l, r = 0, len(s)-1
+        s = list(s)
+        
+        while l<r:
+            while s[l] not in 'AEIOUaeiou' and l<r:
+                l += 1
+            while s[r] not in 'AEIOUaeiou' and l<r:
+                r -= 1
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return ''.join(s)
+        
