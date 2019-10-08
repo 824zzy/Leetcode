84d27fed1b533a555e90c0b4e877a2e49728e393
824zzy <614594368@qq.com>
2019-10-07 23:00:01 -0500

    docs(19 10 7):

---
+++ 187_Repeated_DNA_Sequences_L1.py
@@ -0,0 +1,10 @@
+# Set
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        DNA, ans = set(), []
+        for i in range(len(s)-9):
+            if s[i:i+10] not in DNA:
+                DNA.add(s[i:i+10])
+            else:
+                ans.append(s[i:i+10])
+        return list(set(ans))
---
+++ 3_Math/264_Ugly_NUmber_II_L1.py
@@ -0,0 +1,13 @@
+# Using heap: 304ms
+import heapq
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        N, m, S = [1], 1, set()
+        for _ in range(n):
+            while m in S:
+                m = heapq.heappop(N)
+            S.add(m)
+            for i in 2,3,5:
+                heapq.heappush(N, i*m)
+        return m
+        
