c99a82d6c4698afc72fa309ec8bd7b471027cf26
824zzy <614594368@qq.com>
2020-07-22 23:30:18 -0500

    docs:

---
+++ 1_Array/Basic/1078_Occurrences_After_Bigram_L0.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        text = text.split(' ')
+        # print(text)
+        ans = []
+        for i in range(len(text)-2):
+            if text[i]==first and text[i+1]==second:
+                ans.append(text[i+2])
+        return ans
