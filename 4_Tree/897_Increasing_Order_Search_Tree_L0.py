bb09eb46c1ed03deb3fd548bf15cef6245fea4d7
824zzy <614594368@qq.com>
2020-07-26 17:55:58 -0500

    docs:

--- 4_Tree/897_Increasing_Order_Search_Tree_L0.py
+++ 4_Tree/897_Increasing_Order_Search_Tree_L0.py
@@ -19,3 +19,13 @@ class Solution:
         
         ans = create(tree, 0)
         return ans
+    
+    
+class Solution:
+    def increasingBST(self, root: TreeNode, tail=None) -> TreeNode:
+        if not root: 
+            return tail
+        result = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return result
