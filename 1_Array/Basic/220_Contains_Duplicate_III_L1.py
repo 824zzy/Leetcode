3bdfe05046cd579a8487ad45feb924d8a86a63bf
824zzy <614594368@qq.com>
2020-09-05 18:25:18 -0500

    docs($): n

---
+++ 1_Array/Basic/220_Contains_Duplicate_III_L1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if len(nums)>1000:
+            nums = sorted(nums)
+            for i in range(len(nums)-1):
+                if nums[i]==nums[i+1]:
+                    return True
+            return False
+        for i, n in enumerate(nums):
+            for j in range(i+1, i+1+k):
+                if j<len(nums) and abs(nums[i]-nums[j])<=t:
+                    return True
+        return False
