c4ffab3474563c4228257b956e965abbe3a2859d
824zzy <614594368@qq.com>
2020-09-07 16:18:21 -0500

    docs($): n

--- 2_String/Hash_Table/290_Word_Pattern_L1.py
+++ 2_String/Hash_Table/290_Word_Pattern_L1.py
@@ -1,3 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
+        if not pattern:
+            return False
+        
+        pd, sd = defaultdict(list), defaultdict(list)
+        for i, s in enumerate(str.split()):
+            sd[s].append(i)
+        for i, s in enumerate(pattern):
+            pd[s].append(i)
+            
+        for k, v in pd.items():
+            if v not in sd.values():
+                return False
+        return True
+    
 class Solution:
     def wordPattern(self, pattern: str, str: str) -> bool:
         if len(pattern)!=len(str.split()):
