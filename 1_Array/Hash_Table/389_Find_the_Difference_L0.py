240d75db6701c9686cdbb7d2062851b40a4031cc
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-09-24 22:51:57 -0500

    n

--- 1_Array/Hash_Table/389_Find_the_Difference_L0.py
+++ 1_Array/Hash_Table/389_Find_the_Difference_L0.py
@@ -12,12 +12,9 @@ class Solution:
         cs = dict(Counter(s))
         ct = dict(Counter(t))
         words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        # print(cs, ct)
         for word in words:
             if word in cs and word in ct:
                 if cs[word] != ct[word]:
                     return word
             elif word in cs or word in ct:
                 return word
-        
-        
