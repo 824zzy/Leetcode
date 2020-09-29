4ea5c1d646e68ba35ce7c6f82e0b4b14de2cbf19
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-09-29 11:59:50 -0500

    none

--- 1_Array/SlidingWindow/209_Minimum_Size_Subarray_Sum_L2.py
+++ 1_Array/SlidingWindow/209_Minimum_Size_Subarray_Sum_L2.py
@@ -1,10 +1,11 @@
 class Solution:
     def minSubArrayLen(self, s: int, A: List[int]) -> int:
-        i, ans = 0, len(A)+1
+        i = 0
+        ans = len(A)+1
         for j in range(len(A)):
             s -= A[j]
-            while s<=0:
-                ans = min(ans, j-i+1)
+            while s<=0 and i<j:
+                ans = min(j-i+1, ans)
                 s += A[i]
                 i += 1
         return ans % (len(A)+1)
