f002ca2a88e0d0d509c8fb15b58a13bdf5156b30
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-12-05 23:22:38 -0600

    n

---
+++ 3_Math/Algebra/171_Excel_Sheet_Column_Number_L0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        ans = 0
+        for i,c in enumerate(reversed(s)):
+            ans += (ord(c)-64) * 26 ** i
+        return ans
---
+++ 3_Math/Algebra/172_Factorial_Trailing_Zeroes_L1.py
@@ -0,0 +1,9 @@
+""" count five
+"""
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        ans = 0
+        while n != 0:
+            n = n // 5
+            ans += n
+        return ans
