ea74f9c4920b9ce5e0a22ff42250cccd07e0abbf
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-12-05 23:22:21 -0600

    n

--- 4_Tree/897_Increasing_Order_Search_Tree_L0.py
+++ 4_Tree/897_Increasing_Order_Search_Tree_L0.py
@@ -1,4 +1,14 @@
 class Solution:
+    def increasingBST(self, root: TreeNode, tail=None) -> TreeNode:
+        if not root: 
+            return tail
+        result = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return result
+    
+    
+class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
         tree = []
         def dfs(node):
@@ -19,13 +29,23 @@ class Solution:
         
         ans = create(tree, 0)
         return ans
-    
-    
+
+
 class Solution:
-    def increasingBST(self, root: TreeNode, tail=None) -> TreeNode:
-        if not root: 
-            return tail
-        result = self.increasingBST(root.left, root)
-        root.left = None
-        root.right = self.increasingBST(root.right, tail)
-        return result
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        self.ans = TreeNode(0)
+        self.prev = None
+        self.dummy = self.ans
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            self.ans.val = node.val
+            self.ans.right = TreeNode(0)
+            self.prev = self.ans
+            self.ans = self.ans.right
+            dfs(node.right)
+        
+        dfs(root)
+        self.prev.right = None
+        return self.dummy
