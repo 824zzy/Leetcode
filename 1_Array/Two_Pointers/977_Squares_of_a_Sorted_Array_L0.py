a5c9f63353bd3b6f7679d9010321ebfafb0ef9a3
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-12-18 02:50:25 -0600

    n

--- 1_Array/Two_Pointers/977_Squares_of_a_Sorted_Array_L0.py
+++ 1_Array/Two_Pointers/977_Squares_of_a_Sorted_Array_L0.py
@@ -7,6 +7,7 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         return sorted([n**2 for n in nums])
     
+# Two pointer
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         l, r = 0, len(nums)-1
