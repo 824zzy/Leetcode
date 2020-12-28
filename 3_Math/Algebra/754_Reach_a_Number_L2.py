32d128d392673179040af602a6a743d0e69c9b90
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-12-28 16:09:46 -0600

    n

---
+++ 3_Math/Algebra/754_Reach_a_Number_L2.py
@@ -0,0 +1,17 @@
+"""
+ans target-ans k
+6 1 4
+10 5 5
+15 10 6
+"""
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        ans, k = 0, 0
+        target = abs(target)
+        while ans < target:
+            ans += k
+            k += 1
+        while (ans - target) % 2:
+            ans += k
+            k += 1
+        return k - 1  
