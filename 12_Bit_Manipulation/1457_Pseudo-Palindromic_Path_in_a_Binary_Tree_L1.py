861b8d31e5382738ba2c8f584bd327b082f0c0f8
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2020-12-30 21:21:01 -0600

    arr

--- 12_Bit_Manipulation/1457_Pseudo-Palindromic_Path_in_a_Binary_Tree_L1.py
+++ 12_Bit_Manipulation/1457_Pseudo-Palindromic_Path_in_a_Binary_Tree_L1.py
@@ -3,7 +3,7 @@ class Solution:
         self.ans = 0
         def dfs(node, mask):
             if not node: return
-            mask ^= (1<<node.val)
+            mask ^= (1<<node.val-1)
             if not node.left and not node.right:
                 self.ans += mask==0 or math.log(mask, 2)%1==0
             dfs(node.left, mask)
---
+++ 15_Graph/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-29 23:56:03
+ * @LastEditTime: 2020-12-29 23:56:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Leetcode/15_Graph/README.md
+-->
