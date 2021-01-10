c65eceb4275da95930b9c6a54e095e87abe92e36
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-01-10 00:26:03 -0600

    review

--- 15_Graph/BFS/127_Word_Ladder_L2.py
+++ 15_Graph/BFS/127_Word_Ladder_L2.py
@@ -1,23 +1,23 @@
+# Reduce time complexity by ordina
 class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if not wordList:
-            return 0
-        queue = [(beginWord, 1)]
-        seen = set()
-        wordSet = set(wordList)
+    def ladderLength(self, b: str, e: str, L: List[str]) -> int:
+        if e not in L: return 0
+        queue = [b]
+        masks = defaultdict(list)
+        L = set(L)
+        step = 1
         while queue:
-            curr, dist = queue.pop(0)
-            if curr==endWord:
-                return dist
-            if curr in seen:
-                continue
-            for i in range(len(curr)):
-                for j in range(0, 26):
-                    ordinal = ord('a')+j
-                    next_word = curr[0:i]+chr(ordinal)+curr[i+1:]
-                    if next_word in wordSet:
-                        queue.append((next_word, dist+1))
-            seen.add(curr)
+            for _ in range(len(queue)):
+                curr = queue.pop(0)
+                for i, c in enumerate(curr):
+                    for j in range(26):
+                        ordinal = ord('a')+j
+                        l = curr[:i]+chr(ordinal)+curr[i+1:]
+                        if l in L: 
+                            queue.append(l)
+                            L.remove(l)
+                if e in queue: return step+1
+            step += 1
         return 0
     
 # Reduce time complexity by defaultdict
