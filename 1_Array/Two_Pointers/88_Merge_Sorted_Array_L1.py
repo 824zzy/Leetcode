b948a8f3786bf741c5c89e4979c9194288d5de77
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-01-11 04:01:58 -0600

    BS

---
+++ 1_Array/Two_Pointers/88_Merge_Sorted_Array_L1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """        
+        i, j, k = m-1, n-1, m+n-1
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j>=0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
