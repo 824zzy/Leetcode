cc55bb2851abed520053e0c641949309c904f02f
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-01-16 19:50:46 -0600

    daily

--- 6_Dynamic_Programming/one_dimention/1646_Get_Maximum_in_Generated_Array_L0.py
+++ 6_Dynamic_Programming/one_dimention/1646_Get_Maximum_in_Generated_Array_L0.py
@@ -1,24 +1,8 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        if n==0:
-            return 0
-        dp = [0] * (n+1)
-        dp[0] = 0
-        dp[1] = 1
-        for i in range(n+1):
-            if 2*i<n+1:
-                dp[2*i] = dp[i]
-            if 2*i+1<n+1:
-                dp[2*i+1] = dp[i]+dp[i+1]
+        if n==0: return 0
+        dp = [0, 1] + [0]*(n-1)
+        for i in range(2, n+1):
+            if i%2==0: dp[i] = dp[i//2]
+            else: dp[i] = dp[i//2]+dp[i//2+1]
         return max(dp)
-    
-class Solution:
-    def getMaximumGenerated(self, n: int) -> int:
-        l=[0]*(n+1)
-        if n==0:
-            return 0
-        l[1]=1
-        for i in range(1,(n+1)//2):
-            l[2*i]=l[i]
-            l[2*i + 1]= l[i]+l[i+1]
-        return max(l)
