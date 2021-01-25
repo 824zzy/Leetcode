56a339db9a25feb883c630a74048dc591a65a32e
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-01-25 15:10:45 -0600

    array

--- 1_Array/Basic/1732_Find_the_Highest_Altitude_L0.py
+++ 1_Array/Basic/1732_Find_the_Highest_Altitude_L0.py
@@ -1,8 +1,7 @@
 # easy: prefix sum array
 class Solution:
     def largestAltitude(self, A: List[int]) -> int:
-        prefix = 0
-        h = [0]
+        prefix, h = 0, [0]
         for a in A:
             prefix += a
             h.append(prefix)
