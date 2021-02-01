ac96da047a861015000308d4e615f0db29e91934
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-01-31 19:54:31 -0600

    n

--- 1_Array/Basic/41_First_Missing_Positive_L3.py
+++ 1_Array/Basic/41_First_Missing_Positive_L3.py
@@ -1,9 +1,11 @@
 # Time Complexity: O(N), Space Complexity: O(1)
 class Solution:
     def firstMissingPositive(self, A: List[int]) -> int:
+        # mark outliers
         for i in range(len(A)):
             if A[i]<=0 or A[i]>len(A): A[i] = len(A)+1
         
+        # use abs() to avoid index out of range
         for a in A:
             a = abs(a)
             if a<=len(A) and A[a-1]>=0: A[a-1] *= -1
--- 2_String/Basic/14_Longest_Common_Prefix.py
+++ 2_String/Basic/14_Longest_Common_Prefix.py
@@ -5,8 +5,6 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         ans = ''
         for c in zip(*strs):
-            if len(set(c))!=1:
-                return ans
-            else:
-                ans += c[0]
+            if len(set(c))!=1: return ans
+            else: ans += c[0]
         return ans
