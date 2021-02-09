480b4a850cb1f08803d63ab409a8cef55f76d4d8
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-02-08 18:28:32 -0600

    design

---
+++ 18_Design/284_Peeking_Iterator_L1.py
@@ -0,0 +1,8 @@
+"""Description
+Author: your name
+Date: 2021-02-08 18:25:13
+LastEditTime: 2021-02-08 18:25:16
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /Leetcode/18_Design/284_Peeking_Iterator_L1.py
+"""
