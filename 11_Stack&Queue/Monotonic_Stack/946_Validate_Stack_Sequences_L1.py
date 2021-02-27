5788162311b5fde1cd61a2ec5430906d8c17e23e
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-02-27 02:52:17 -0600

    stack

---
+++ 11_Stack&Queue/Monotonic_Stack/946_Validate_Stack_Sequences_L1.py
@@ -0,0 +1,10 @@
+# special monotonic stack compared with popped stack.
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stk = []
+        for p in pushed:
+            stk.append(p)
+            while stk and stk[-1]==popped[0]:
+                popped.pop(0)
+                stk.pop()
+        return stk==[]
