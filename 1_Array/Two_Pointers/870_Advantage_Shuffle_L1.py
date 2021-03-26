7191c4c66301cd437e9a518e2049634b19831755
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-03-26 00:23:31 -0500

    arr

---
+++ 1_Array/Two_Pointers/870_Advantage_Shuffle_L1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
+        tmpB = B
+        A = sorted(A)
+        B = sorted(B)
+        l, r = 0, len(A)-1
+        M = defaultdict(list)
+        for a in A:
+            if l>r: break
+            if a>B[l]:
+                M[B[l]].append(a)
+                l += 1
+            else:
+                M[B[r]].append(a)
+                r -= 1
+
+        ans = []
+        for b in tmpB:
+            a = M[b].pop(0)
+            ans.append(a)
+        return ans
