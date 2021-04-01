9bef1d4bb1dbdd65e6257ae634447e1c44fd094f
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-04-01 17:31:33 -0500

    linked list

--- 234_Palindrome_Linked_List_L1.py
+++ 234_Palindrome_Linked_List_L1.py
@@ -1,45 +1,35 @@
-""" naive stack solution
+""" Tiem complexity: O(N). Space complexity: O(N)
+
 """
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        stack = []
-        dummy = head
-        while dummy:
-            stack.append(dummy.val)
-            dummy = dummy.next
-        for i, j in zip(stack, reversed(stack)):
-            if i != j:
-                return False
-        return True
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        s = []
+        while head:
+            s.append(head.val)
+            head = head.next
+        return s==s[::-1]
 
 
-""" another solution
+""" Tiem complexity: O(N). Space complexity: O(1)
+Use fast slow pointer to split linked list
+reverse the second half and compare.
 """
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        
-        stack = []
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head or not head.next: return True
         fast = slow = head
-        
-        while fast and fast.next:
-            stack.append(slow.val)
-            fast, slow = fast.next.next, slow.next
-        
-        if fast:
+        while fast.next and fast.next.next:
             slow = slow.next
-        
-        while slow:
-            if stack.pop() != slow.val:
-                return False
+            fast = fast.next.next
+        last = slow.next
+        pre = head
+        while last.next:
+            tmp = last.next
+            last.next = tmp.next
+            tmp.next = slow.next
+            slow.next = tmp
+        while slow.next:
             slow = slow.next
-        
+            if pre.val!=slow.val: return False
+            pre = pre.next
         return True
-        
