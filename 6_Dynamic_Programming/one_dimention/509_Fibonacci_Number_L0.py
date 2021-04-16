577f533d83f8d5518436cff14e2f80efa9c428d2
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-04-16 00:38:13 -0500

    dp

--- 6_Dynamic_Programming/one_dimention/509_Fibonacci_Number_L0.py
+++ 6_Dynamic_Programming/one_dimention/509_Fibonacci_Number_L0.py
@@ -1,9 +1,16 @@
+# Iteration
 class Solution:
-    def fib(self, N: int) -> int:
-        if N==0:
-            return 0
-        dp = [0] * (N+1)
-        dp[1] = 1
-        for i in range(2, N+1):
+    def fib(self, n: int) -> int:
+        if n<2: return n
+        dp = [0] * (n+1)
+        dp[0], dp[1] = 0, 1
+        for i in range(2, n+1):
             dp[i] = dp[i-1]+dp[i-2]
-        return dp[N]
+        return dp[-1]
+    
+# Recursive 
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==1: return 1
+        elif n==0: return 0
+        return self.fib(n-1)+self.fib(n-2)
