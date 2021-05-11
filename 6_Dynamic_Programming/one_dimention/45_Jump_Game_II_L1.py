13fd549cb2dafc33026659a91659167be32fd87e
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-05-10 19:43:58 -0500

    dp

---
+++ 6_Dynamic_Programming/one_dimention/45_Jump_Game_II_L1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def jump(self, A: List[int]) -> int:
+        dp = [float('inf')] * len(A)
+        dp[0] = 0
+        for i in range(len(A)):
+            for j in range(1, A[i]+1):
+                if i+j<len(A): dp[i+j] = min(dp[i+j], dp[i]+1)
+        return dp[-1]
