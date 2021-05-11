193776393106e5a271d0857f7851e42ed585d3c5
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-05-11 01:53:00 -0500

    arr

---
+++ 1_Array/Basic/1791_Find_Center_of_Star_Graph_L0.py
@@ -0,0 +1,12 @@
+"""
+only need to compare edges[0] and edges[1]
+"""
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        cnt = Counter(edges[0]+edges[1])
+        for k, v in cnt.items():
+            if v==2: return k
+
+class Solution:
+    def findCenter(self, e):
+        return (set(e[0]) & set(e[1])).pop()
