20b422bd3544d688bc4e463002392f83e7923f46
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-06-02 18:40:05 -0500

    dp etc

--- 15_Graph/Basic/463_Island_Perimeter_L0.py
+++ 15_Graph/Basic/463_Island_Perimeter_L0.py
@@ -1,46 +1,10 @@
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
+    def islandPerimeter(self, A: List[List[int]]) -> int:
         ans = 0
-        m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==1:
-                    if i-1<0:
-                        ans += 1
-                    elif grid[i-1][j]==0:
-                        ans += 1
-                    if i+1>=m:
-                        ans += 1
-                    elif grid[i+1][j]==0:
-                        ans += 1
-                    if j-1<0:
-                        ans += 1
-                    elif grid[i][j-1]==0:
-                        ans += 1
-                    if j+1>=n:
-                        ans += 1
-                    elif grid[i][j+1]==0:
-                        ans += 1
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if A[i][j]:
+                    ans += 4
+                    if i>0 and A[i-1][j]: ans -= 2
+                    if j>0 and A[i][j-1]: ans -= 2
         return ans
-
-
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        
-        m, n, nums, conn = len(grid), len(grid[0]), 0, 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    nums += 1
-                    if i>0 and grid[i-1][j]==1:
-                        conn += 1
-                    if i<m-1 and grid[i+1][j] ==1:
-                        conn += 1
-                    if j>0 and grid[i][j-1]==1:
-                        conn += 1
-                    if j<m-1 and grid[i][j+1]==1:
-                        conn += 1
-        
-        return 4*nums-conn
--- 15_Graph/DFS/695_Max_Area_of_Island.py
+++ 15_Graph/DFS/695_Max_Area_of_Island.py
@@ -1,4 +1,4 @@
-""" L0: Graph DFS template
+""" L1: Graph DFS template
 """
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
