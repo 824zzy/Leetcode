8273473247421e5ffc58d5ce1dba54716bb7ee0b
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-06-14 19:56:17 -0500

    dp

--- 6_Dynamic_Programming/two_dimention/221_Maximal_Square.py
+++ 6_Dynamic_Programming/two_dimention/221_Maximal_Square.py
@@ -1,8 +1,16 @@
-"""Description
-Author: your name
-Date: 2021-06-14 19:28:16
-LastEditTime: 2021-06-14 19:28:16
-LastEditors: Please set LastEditors
-Description: In User Settings Edit
-FilePath: /Leetcode/6_Dynamic_Programming/two_dimention/221_Maximal_Square.py
+"""L1: 2D DP
+dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])+1
 """
+class Solution:
+    def maximalSquare(self, A: List[List[str]]) -> int:
+        if not A or not A[0]: return 0
+        dp = [[0 for j in range(len(A[0])+1)] for i in range(len(A)+1)]
+        ma = 0
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if i==0 or j==0: dp[i][j] = int(A[i][j])
+                else:
+                    if A[i][j]=='1':
+                        dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])+1
+                ma = max(ma, dp[i][j])
+        return ma*ma
