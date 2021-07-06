434b46ff7dba0eeddd6aae521694cb4ba775950d
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-07-05 21:25:00 -0500

    r

--- 3_Math/566_Reshape_the_Matrix_L2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
-        if r*c > len(nums)*len(nums[0]):
-            return nums
-        nr, nc = len(nums), len(nums[0])
-        ans = [[None for _ in range(c)] for _ in range(r)]
-        for i in range(nr*nc):
-            ans[int(i/c)][int(i%c)] = nums[int(i/nc)][int(i%nc)]
-        return ans
