2e0fc5640c268babeea8632a85ef98b448630313
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-07-07 18:14:02 -0500

    heap

---
+++ 10_Heap(PriorityQueue)/378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -0,0 +1,11 @@
+""" L1
+use k as threshold to maintain heap H
+"""
+class Solution:
+    def kthSmallest(self, A: List[List[int]], k: int) -> int:
+        H = []
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                heapq.heappush(H, -A[i][j])
+                if len(H)>k: heapq.heappop(H)
+        return -heapq.heappop(H)
