76a2b99eaf5864e5d4103454c97948c7d607066b
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-07-15 19:32:53 -0500

    bisect

---
+++ 8_BinarySearch/bisect/611_Valid_Triangle_Number.py
@@ -0,0 +1,12 @@
+""" L1
+three pointer variance: use i, j as two pointer and pos to find minimum length of third side.
+"""
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                pos = bisect_left(nums, nums[i]+nums[j])
+                ans += max(0, pos-1-j)
+        return ans
