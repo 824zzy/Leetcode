f66c3287d168bca83710a1b36444e70087c1c32f
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-08-24 19:00:23 -0500

    sw

--- 1_Array/SlidingWindow/1004_Max_Consecutive_Ones_III.py
+++ 1_Array/SlidingWindow/1004_Max_Consecutive_Ones_III.py
@@ -1,4 +1,4 @@
-""" L2
+""" L1
 move left pointer when k=0 and move one step further.
 """
 class Solution:
@@ -11,5 +11,20 @@ class Solution:
                     while A[i]!=0: i += 1
                     i += 1
                 else: k -= 1
+            ans = max(ans, j-i+1)
+        return ans
+
+# another solution with the same idea
+class Solution:
+    def longestOnes(self, A: List[int], k: int) -> int:
+        ans = 0
+        i = 0
+        for j in range(len(A)):
+            if A[j]==0:
+                k -= 1
+                if k<0:
+                    if A[i]==0: k + 1
+                    while A[i]==1: i += 1
+                    i += 1
             ans = max(ans, j-i+1)
         return ans
---
+++ 1_Array/SlidingWindow/1358_Number_of_Substrings_Containing_All_Three_Characters.py
@@ -0,0 +1,15 @@
+""" L2:
+tricky line to count all the legit substrings: ans += i
+"""
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        ans = 0
+        i = 0
+        seen = {'a': 0, 'b': 0, 'c': 0}
+        for j in range(len(s)):
+            seen[s[j]] += 1
+            while all(seen.values()):
+                seen[s[i]] -= 1
+                i += 1
+            ans += i
+        return ans
