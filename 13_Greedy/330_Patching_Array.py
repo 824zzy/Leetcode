7203cd11c24a26617eaa0a10f34344b16b9ebd75
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-08-29 18:04:19 -0500

    greedy

---
+++ 13_Greedy/330_Patching_Array.py
@@ -0,0 +1,13 @@
+""" L4: cnblogs.com/grandyang/p/5165821.html
+"""
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss, ans, i = 1, 0, 0
+        while miss<n:
+            if i<len(nums) and nums[i]<=miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                ans += 1
+        return ans
