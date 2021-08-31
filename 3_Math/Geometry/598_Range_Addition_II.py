62c02563617641ebd776d1e35b6b85badcdb3143
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-08-30 19:48:14 -0500

    math

---
+++ 3_Math/Geometry/598_Range_Addition_II.py
@@ -0,0 +1,8 @@
+""" L1
+find minimum value of top left corner
+"""
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        if not ops: return m*n
+        min_m, min_n = min([o[0] for o in ops]), min([o[1] for o in ops])
+        return min_m*min_n
