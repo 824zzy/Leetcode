18ebb3cbf3e611cfbea877a5fe2d1a9b7fac1f13
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-14 15:45:24 -0500

    two pointer

---
+++ 1_Array/Two_Pointers/917_Reverse_Only_Letters.py
@@ -0,0 +1,14 @@
+""" L1
+two pointers to find and swap letters.
+"""
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        l, r = 0, len(s)-1
+        while l<r:
+            while l<r and not s[l].isalpha(): l += 1
+            while l<r and not s[r].isalpha(): r -= 1
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return ''.join(s)
