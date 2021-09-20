a2a639f059d3cfac9185918cfeac43edfc7a118b
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-20 02:23:40 -0500

    simulation

---
+++ 2_String/Simulation/1275_Find_Winner_on_a_Tic_Tac_Toe_Game.py
@@ -0,0 +1,22 @@
+""" L1
+Simulate game board
+"""
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        N = 3
+        rows = [0] * N
+        cols = [0] * N
+        mainDiag = 0
+        antiDiag = 0
+        
+        player = 1
+        for r, c in moves:
+            rows[r] += player
+            cols[c] += player
+            if r == c: mainDiag += player
+            if r + c == N - 1: antiDiag += player
+            if abs(rows[r]) == N or abs(cols[c]) == N or abs(mainDiag) == N or abs(antiDiag) == N:
+                return "A" if player == 1 else "B"
+            player = -player
+            
+        return "Draw" if len(moves) == N*N else "Pending"
