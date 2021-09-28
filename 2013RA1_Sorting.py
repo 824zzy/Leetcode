d154b9a34efbfdb4f393ac499e14bcb25035f11c
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-27 23:44:49 -0500

    kickstart

--- 2013RA1_Sorting.py
+++ 2013RA1_Sorting.py
@@ -1,11 +1,19 @@
 """ L1
 simulate insert sort and cound number of insert
 """
+import heapq
 def Sorting(A):
     ans = 0
     A = [int(a) for a in A.split()]
-    print(A)
-    return ans
+    ans = []
+    H1, H2 = [], []
+    for a in A:
+        if a%2: heapq.heappush(H1, a)
+        else: heapq.heappush(H2, -a)
+    for a in A:
+        if a%2: ans.append(str(heapq.heappop(H1)))
+        else: ans.append(str(-1*heapq.heappop(H2)))
+    return " ".join(ans)
         
 t = int(input())
 for i in range(1, t+1):
