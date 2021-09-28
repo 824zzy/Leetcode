a28fd21292cbd3c4d7d11f462c90b6537c1f638b
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-09-27 22:30:06 -0500

    string&math

---
+++ 2_String/Simulation/929_Unique_Email_Address.py
@@ -0,0 +1,14 @@
+""" L0
+Basic string usage
+"""
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        seen = set()
+        for e in emails:
+            local, domain = e.split('@')
+            local = local.split('+')[0].replace('.', '')
+            seen.add('@'.join([local, domain]))
+        return len(seen)
+
+s = Solution()
+print(s.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
