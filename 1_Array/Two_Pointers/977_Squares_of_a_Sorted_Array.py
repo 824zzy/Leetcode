6661679c24abc3eb2d0e156066d933f4e1aece5e
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-10-11 02:17:15 -0500

    review

---
+++ 1_Array/Two_Pointers/977_Squares_of_a_Sorted_Array.py
@@ -0,0 +1,15 @@
+""" L0: https://leetcode.com/problems/squares-of-a-sorted-array/
+find larger absolute value of two pointers
+"""
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        ans = []
+        l, r = 0, len(A)-1
+        while l<=r:
+            if abs(A[l])>abs(A[r]):
+                ans.append(A[l]**2)
+                l += 1
+            else:
+                ans.append(A[r]**2)
+                r -= 1
+        return ans[::-1]
