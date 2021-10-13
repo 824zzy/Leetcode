0fb34cbd64c737b2e06c5d0099b0935c6fa3b1d3
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-10-13 00:26:17 -0500

    daily

--- 8_BinarySearch/bisect/658_Find_K_Cloest_Elements.py
+++ 8_BinarySearch/bisect/658_Find_K_Cloest_Elements.py
@@ -1,10 +1,10 @@
-""" L2
-insert x into arr and use two pointer to find K closest elements.
+""" L1: https://leetcode.com/problems/find-k-closest-elements/
+find idx by bisect_left and use two pointers to find the k closest elements
 """
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         arr = [-float('inf')] + arr + [float('inf')]
-        idx = bisect.bisect(arr,x)
+        idx = bisect.bisect_left(arr,x)
         i, j = idx-1, idx
         while 1:
             if abs(arr[i]-x)<=abs(arr[j]-x): i -= 1
