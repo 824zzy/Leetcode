8061491c998c8aa7f4b5f628ad336445e9cb4d39
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-10-13 01:00:20 -0500

    daily

---
+++ 4_Tree/DFS/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -0,0 +1,13 @@
+""" L0: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+"""
+class Solution:
+    def buildTree(self, I: List[int], P: List[int]) -> Optional[TreeNode]:
+        def dfs(I, P):
+            if not I and not P: return None
+            v = P.pop()
+            node = TreeNode(v)
+            idx = I.index(v)
+            node.left = dfs(I[:idx], P[:idx])
+            node.right = dfs(I[idx+1:], P[idx:])
+            return node
+        return dfs(I, P)
