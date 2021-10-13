f18a71893a0f5a3aa82d1b759df57f786e3850a0
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-10-13 00:21:30 -0500

    daily

---
+++ 4_Tree/DFS/1008_Construct_Binary_Search_Tree_from_Preorder_Traversal.py
@@ -0,0 +1,15 @@
+""" L0: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+dfs+bisect to seperate left and right children.
+"""
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        def dfs(vals):
+            if not vals: return None
+            v = vals.pop(0)
+            node = TreeNode(v)
+            idx = bisect_left(vals, v)
+            node.left = dfs(vals[:idx])
+            node.right = dfs(vals[idx:])
+            return node
+        
+        return dfs(preorder)
