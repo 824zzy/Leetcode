c216a1bccb2afa9396c3c15c655c04b973cf0178
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-10-19 15:21:33 -0500

    monotonic

--- 11_Stack&Queue/Monotonic_Stack/503_Next_Greater_Element_II.py
+++ 11_Stack&Queue/Monotonic_Stack/503_Next_Greater_Element_II.py
@@ -5,7 +5,11 @@ Loop twice, we can get the Next Greater Number of a circular array
 class Solution:
     def nextGreaterElements(self, A):
         stack, res = [], [-1] * len(A)
-        for i in range(len(A)) * 2:
+        for i in range(len(A)):
+            while stack and (A[stack[-1]] < A[i]):
+                res[stack.pop()] = A[i]
+            stack.append(i)
+        for i in range(len(A)):
             while stack and (A[stack[-1]] < A[i]):
                 res[stack.pop()] = A[i]
             stack.append(i)
