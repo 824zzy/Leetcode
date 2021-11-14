659ef39a7429668b2ad64cbe05a814130b0cfebd
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-14 02:28:23 -0600

    review

---
+++ HashTable/String/242_Valid_Anagram.py
@@ -0,0 +1,10 @@
+""" L0: https://leetcode.com/problems/valid-anagram/
+count or sorted characters 
+"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
