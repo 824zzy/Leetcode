45d0881602e0a0c0fa00d186ea3b69fe4ad60a0e
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-21 03:26:44 -0600

    contest

---
+++ 8_BinarySearch/Bisect/2080_Range_Frequency_Queries_L2.py
@@ -0,0 +1,13 @@
+""" https://leetcode.com/problems/range-frequency-queries/
+binary serach + hash table
+"""
+class RangeFreqQuery:
+
+    def __init__(self, arr: List[int]):
+        self.cnt = defaultdict(list)
+        for i, a in enumerate(arr): self.cnt[a].append(i)
+
+    def query(self, l: int, r: int, val: int) -> int:
+        i = bisect_right(self.cnt[val], l-1)
+        j = bisect_right(self.cnt[val], r)
+        return j-i
