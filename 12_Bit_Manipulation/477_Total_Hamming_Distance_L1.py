b52ca9cfabbc7bf70d53d148e468faeaa7d5c95d
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-21 03:26:18 -0600

    review

--- 12_Bit_Manipulation/477_Total_Hamming_Distance_L1.py
+++ 12_Bit_Manipulation/477_Total_Hamming_Distance_L1.py
@@ -1,5 +1,6 @@
 """ https://leetcode.com/problems/total-hamming-distance/
 '{0:032b}'.format: a short way to format the integer to a 32 bit binary
+In each index, the total number of disn.count('1') * n.count('0')
 """
 class Solution:
     def totalHammingDistance(self, nums: List[int]) -> int:
