47a19bcb6ff71d90922782e74b16dd10ea184df8
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-21 18:55:39 -0600

    daily challenge

--- 9_DFS/Tree/450_Delete_Node_in_a_BST_L1.py
+++ 9_DFS/Tree/450_Delete_Node_in_a_BST_L1.py
@@ -2,21 +2,17 @@
 node.right = dfs(node.right, tmp.val) // Call the recursive function in the right subtree to delete the node with the smallest value
 """
 class Solution:
-    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         def dfs(node, k):
             if not node: return None
-            if node.val==k:
-                if not node.left or not node.right:
-                    return node.left if node.left else node.right
-                else:
-                    tmp = node.right
-                    while tmp.left: tmp = tmp.left
-                    node.val = tmp.val
-                    node.right = dfs(node.right, tmp.val)
-            elif node.val>k:
-                node.left = dfs(node.left, k)
+            if node.val<k: node.right = dfs(node.right, k)
+            elif node.val>k: node.left = dfs(node.left, k)
             else:
-                node.right = dfs(node.right, k)
+                if not node.left or not node.right: return node.left or node.right
+                tmp = node.left
+                while tmp.right: tmp = tmp.right
+                node.val = tmp.val
+                node.left = dfs(node.left, tmp.val)
             return node
-
+        
         return dfs(root, key)
