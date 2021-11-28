5a31cd62136551fc33c1d225d82e2616ceb4c6a2
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-28 03:19:58 -0600

    Refactor

---
+++ 0_Basic/HashTable/2085_Count_Common_Words_With_One_Occurrence_L0.py
@@ -0,0 +1,7 @@
+""" https://leetcode.com/problems/count-common-words-with-one-occurrence/
+"""
+class Solution:
+    def countWords(self, W1: List[str], W2: List[str]) -> int:
+        A = [k for k, v in Counter(W1).items() if v==1]
+        B = [k for k, v in Counter(W2).items() if v==1]
+        return sum([1 for a in A if a in B])
