998445bb44372b2289ec5a693e12ae1dedaa21ce
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-11-27 18:51:33 -0600

    daily challenge

---
+++ 5_Backtracking/Array/797_All_Paths_From_Source_to_Target_L1.py
@@ -0,0 +1,14 @@
+""" https://leetcode.com/problems/all-paths-from-source-to-target/
+go over the DAG using back tracking
+"""
+class Solution:
+    def allPathsSourceTarget(self, G: List[List[int]]) -> List[List[int]]:
+        ans, stk = [], []
+        def dfs(i):
+            if i==len(G)-1: return ans.append(stk+[i].copy())
+            stk.append(i)
+            for j in G[i]: dfs(j)
+            stk.pop()
+        
+        dfs(0)
+        return ans
