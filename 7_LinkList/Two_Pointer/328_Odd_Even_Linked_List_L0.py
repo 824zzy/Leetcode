0f1186ced01defd977d20780362f141d1e8a913c
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-01 20:27:36 -0600

    daily challenge

---
+++ 7_LinkList/Two_Pointer/328_Odd_Even_Linked_List_L0.py
@@ -0,0 +1,16 @@
+""" https://leetcode.com/problems/odd-even-linked-list/
+straight-forward solution using two pointer
+"""
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return head
+        ans = node = ListNode(next=head)
+        odd, even = node.next, node.next.next
+        pre = even
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            even, odd = even.next. odd.next
+        odd.next = pre
+        return ans.next
+    
