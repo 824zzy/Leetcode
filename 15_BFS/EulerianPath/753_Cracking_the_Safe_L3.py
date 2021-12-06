55cd22c8a5f4d18f99e9766b0bc79f536395e055
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-06 01:51:49 -0600

    eluer bless

---
+++ 15_BFS/EulerianPath/753_Cracking_the_Safe_L3.py
@@ -0,0 +1,23 @@
+""" https://leetcode.com/problems/cracking-the-safe/
+modified iterative version of:
+https://leetcode.com/problems/cracking-the-safe/discuss/1612932/Fast-Python-Hierholzer's
+"""
+class Solution:
+    def crackSafe(self, n: int, k: int) -> str:
+        if n==1: return ''.join(map(str, range(k)))
+            
+        P = list(product(range(k), repeat=n))
+        nodes = [''.join(map(str, p)) for p in P]
+        
+        G = defaultdict(list)
+        for node in nodes:
+            G[node[:-1]].append(node[1:])
+        
+        ans = []
+        stk = ['0'*(n-1)]
+        while stk:
+            while G[stk[-1]]:
+                stk.append(G[stk[-1]].pop())
+            ans.append(stk.pop())
+        ans.reverse()
+        return ''.join([ans[i][0] for i in range(len(ans)-1)]+[ans[-1]])
