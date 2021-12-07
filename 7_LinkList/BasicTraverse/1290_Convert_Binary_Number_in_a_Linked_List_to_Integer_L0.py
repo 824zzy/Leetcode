471e880aba4d231ba60880954b1e9236ffaff867
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-06 20:34:02 -0600

    daily challenge

--- 7_LinkList/BasicTraverse/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer_L0.py
+++ 7_LinkList/BasicTraverse/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer_L0.py
@@ -1,3 +1,14 @@
+""" https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+"""
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        ans = 0
+        while head:
+            ans = 2*ans + head.val
+            head = head.next
+        return ans
+    
+    
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         s = ''
