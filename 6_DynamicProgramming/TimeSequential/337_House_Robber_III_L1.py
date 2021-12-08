6cf0b05380525334da223cc19be479daf068235c
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-07 20:26:33 -0600

    daily challenge

--- 6_DynamicProgramming/TimeSequential/337_House_Robber_III_L1.py
+++ 6_DynamicProgramming/TimeSequential/337_House_Robber_III_L1.py
@@ -5,6 +5,19 @@ use canRob to control the dp
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         @lru_cache(None)
+        def dfs(node):
+            if not node: return 0
+            ans = node.val
+            if node.left: ans += dfs(node.left.left) + dfs(node.left.right)
+            if node.right: ans += dfs(node.right.left) + dfs(node.right.right)
+            return max(ans, dfs(node.left)+dfs(node.right))
+        
+        return dfs(root)
+    
+    
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        @lru_cache(None)
         def dfs(node, canRob):
             if not node: return 0
             if canRob: return max(node.val+dfs(node.left, False)+dfs(node.right, False), 
--- 9_DFS/Tree/563_Binary_Tree_Tilt_L0.py
+++ 9_DFS/Tree/563_Binary_Tree_Tilt_L0.py
@@ -1,9 +1,10 @@
+""" https://leetcode.com/problems/binary-tree-tilt/
+"""
 class Solution:
     def findTilt(self, root: TreeNode) -> int:
         self.ans = 0
         def dfs(node: TreeNode) -> Node:
-            if not node:
-                return 0
+            if not node: return 0
             left = dfs(node.left) 
             right = dfs(node.right)
             self.ans += abs(left-right)
