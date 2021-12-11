3513f20e05919203fba1b4c4462d8b70b3ae37b7
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-10 22:46:40 -0600

    daily challenge

---
+++ 3_Math/Algebra/GreatestCommonDivisor/878_Nth_Magical_Number_L2.py
@@ -0,0 +1,14 @@
+""" https://leetcode.com/problems/nth-magical-number/
+binary search + math(inclusive-exclusive principle)
+
+use a*b//gcd(a, b) to find the joint set.
+"""
+class Solution:
+    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
+        ab = a*b//gcd(a, b)
+        l, r = min(a, b), n*min(a, b)
+        while l<r:
+            m = (l+r)//2
+            if m//a+m//b-m//ab>=n: r = m
+            else: l = m + 1
+        return l%(10**9+7)
