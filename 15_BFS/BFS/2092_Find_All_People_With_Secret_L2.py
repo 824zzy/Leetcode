933e9402d897ebe44ad642212e9ec3475d5a8514
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-20 01:17:15 -0600

    weekly contest

---
+++ 15_BFS/BFS/2092_Find_All_People_With_Secret_L2.py
@@ -0,0 +1,21 @@
+""" https://leetcode.com/problems/find-all-people-with-secret/
+"""
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        can = {0, firstPerson}
+        for _, grp in groupby(sorted(meetings, key=lambda x: x[2]), key=lambda x: x[2]): 
+            Q = set()
+            G = defaultdict(list)
+            for x, y, _ in grp:
+                G[x].append(y)
+                G[y].append(x)
+                if x in can: Q.add(x)
+                if y in can: Q.add(y)
+            Q = deque(Q)
+            while Q:
+                x = Q.popleft()
+                for y in G[x]:
+                    if y not in can:
+                        can.add(y)
+                        Q.append(y)
+        return can
