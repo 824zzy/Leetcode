8fc9c87eaba560d9dda04238d0928b41deda9e38
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-24 03:17:13 -0600

    refactor

---
+++ 13_Greedy/Interval_Problem/56_Merge_Intervals_L1.py
@@ -0,0 +1,9 @@
+""" https://leetcode.com/problems/merge-intervals
+"""
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for x, y in sorted(intervals): 
+            if not ans or ans[-1][1] < x: ans.append([x, y]) # new interval 
+            else: ans[-1][1] = max(ans[-1][1], y) # overlapped 
+        return ans 
