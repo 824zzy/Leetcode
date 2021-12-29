f9434799afaef4cede7c0b8f65421609adfb5fab
zzy824 <zhengyuan.zhu@mavs.uta.edu>
2021-12-29 03:49:51 -0600

    review

---
+++ 10_DynamicProgramming/LongestSubsequence/1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome_L1.py
@@ -0,0 +1,12 @@
+""" https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+variance of LCS
+"""
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        @lru_cache(None)
+        def dfs(i, j):
+            if i >= j: return 0
+            if s[i] == s[j]: return dfs(i+1, j-1)
+            return 1 + min(dfs(i+1, j), dfs(i, j-1))
+        
+        return dfs(0, len(s) - 1)
