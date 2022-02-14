5b2dac29ed578abdefbf10802aa10df98fe13755
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-02-14 12:11:09 -0600

    LIS

---
+++ 6_BinarySearch/Bisect/1964_Find_the_Longest_Valid_Obstacle_Course_at_Each_Position_L1.py
@@ -0,0 +1,13 @@
+""" https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/
+The same as 300.
+"""
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, A: List[int]) -> List[int]:
+        vals = []
+        ans = []
+        for x in A: 
+            k = bisect_right(vals, x)
+            ans.append(k+1)
+            if k == len(vals): vals.append(x)
+            else: vals[k] = x
+        return ans
