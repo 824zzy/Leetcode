528e9662e1e509685599eb3c13a80b5c947e636a
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-02-18 09:53:55 -0600

    daily challenge

---
+++ 11_Stack/Monotonic_Stack/OneDimentional/402_Remove_K_Digits_L1.py
@@ -0,0 +1,16 @@
+""" https://leetcode.com/problems/remove-k-digits/
+1. maintain a monotonic increasing stack for finding the smalllest possible integer
+2. the poped element count reach to k then stop poping
+3. the answer will be the first least elements leave in the stask
+"""
+class Solution:
+    def removeKdigits(self, A: str, k: int) -> str:
+        if k>=len(A): return '0'
+        
+        stk = []
+        for i, x in enumerate(A):
+            while stk and stk[-1]>x and k:
+                stk.pop()
+                k -= 1
+            stk.append(x)
+        return str(int(''.join(stk[:-k or None])))
