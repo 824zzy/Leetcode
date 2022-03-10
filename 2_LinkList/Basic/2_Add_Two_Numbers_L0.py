560c51798ad95e602f7c4d4315cd204504d929cd
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-03-10 09:47:58 -0600

    linked list daily challenge

---
+++ 2_LinkList/Basic/2_Add_Two_Numbers_L0.py
@@ -0,0 +1,19 @@
+""" https://leetcode.com/problems/add-two-numbers/
+traverse l1 and l2 while using carry
+"""
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        ans = node = ListNode(-1)
+        carry = 0
+        while l1 or l2 or carry:
+            a = b = 0
+            if l1:
+                a = l1.val
+                l1 = l1.next
+            if l2:
+                b = l2.val
+                l2 = l2.next
+            carry, s = divmod(a+b+carry, 10)
+            node.next = ListNode(s)
+            node = node.next
+        return ans.next
