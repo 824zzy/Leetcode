ec0fd2582a81c3f872a508cb6139d0ce96165e6d
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-03-19 22:26:02 -0500

    daily

---
+++ 5_SlidingWindow/DynamicWindow/159_Longest_Substring_with_At_Most_Two_Distinct_Characters_Lx.py
@@ -0,0 +1,3 @@
+""" https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
+TODO: locked problem
+"""
---
+++ 5_SlidingWindow/DynamicWindow/3_Longest_Substring_Without_Repeating_Characters_L1.py
@@ -0,0 +1,15 @@
+""" https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Use a dictionary seen to keep track of visited characters and their last positions in string. 
+While moving a pointer i forward, if s[i] has seen before, 
+move anchor k (forward only) to 1 + seen[s[i]] to make sure no duplicates between k and i.
+"""
+class Solution:
+    def lengthOfLongestSubstring(self, A: str) -> int:
+        i = 0
+        ans = 0
+        seen = {}
+        for j in range(len(A)):
+            if A[j] in seen: i = max(i, seen[A[j]]+1)
+            ans = max(ans, j-i+1)
+            seen[A[j]] = j
+        return ans
