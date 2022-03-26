d13fcc672d5e7cb6f0be2dbc41b978af791958b8
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-03-26 02:12:11 -0500

    random practice

---
+++ 5_SlidingWindow/DynamicWindow/30_Substring_with_Concatentation_of_All_Words_L2.py
@@ -0,0 +1,20 @@
+""" https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+apply l times sliding window to find all substrings
+"""
+class Solution:
+    def findSubstring(self, A: str, words: List[str]) -> List[int]:
+        l = len(words[0])
+        n = len(words)
+        words = Counter(words)
+        ans = []
+        for i in range(l):
+            cnt = Counter()
+            ii = i
+            for j in range(i, len(A), l):
+                word = A[j:j+l]
+                cnt[word] += 1
+                while cnt[word]>words.get(word, 0):
+                    cnt[A[ii:ii+l]] -= 1
+                    ii += l
+                if j+l-ii==l*n: ans.append(ii)
+        return ans
