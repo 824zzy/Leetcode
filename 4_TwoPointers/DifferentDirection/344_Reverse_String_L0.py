740d033d50fe833e820ac7b8f5af0635bfa8ae9a
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-03-31 19:10:56 -0500

    two pointer

---
+++ 4_TwoPointers/DifferentDirection/344_Reverse_String_L0.py
@@ -0,0 +1,8 @@
+""" https://leetcode.com/problems/reverse-string/
+"""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        l, r = 0, len(s)-1
+        while l<r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
