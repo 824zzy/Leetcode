4c2a30fe1ef2d5774082ffeda29951e165584346
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-05-09 22:19:18 -0500

    daily challenge

---
+++ 0_Basic/String/949_Largest_Time_for_Given_Digits_L0.py
@@ -0,0 +1,10 @@
+""" https://leetcode.com/problems/largest-time-for-given-digits/
+find and sort all possible time by permutation then find the maximal one
+"""
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        A = [[str(a)+str(b), str(c)+str(d)] for a, b, c ,d in list(permutations(A))]
+        A.sort(reverse=True)
+        for x, y in A:
+            if int(x)<=23 and int(y)<60: return ':'.join([str(x), str(y)])
+        return ''
