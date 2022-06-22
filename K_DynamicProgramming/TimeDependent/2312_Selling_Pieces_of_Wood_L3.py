d56bf61460b5b33caae64e7e5701fb06a8f6279d
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-06-22 16:15:03 -0500

    update dp

--- K_DynamicProgramming/TimeDependent/2312_Selling_Pieces_of_Wood_L3.py
+++ K_DynamicProgramming/TimeDependent/2312_Selling_Pieces_of_Wood_L3.py
@@ -1,5 +1,10 @@
 """ https://leetcode.com/problems/selling-pieces-of-wood/
-3D DP: cut across all possible height or width of the piece
+3D DP: cut across all possible height or width of the piece.
+For saving time, we only need to try i / 2 and j / 2 cuts because of the symmetry property.
+
+
+
+Time complexity: O(n^3)
 """
 class Solution:
     def sellingWood(self, m: int, n: int, A: List[List[int]]) -> int:
@@ -12,10 +17,10 @@ class Solution:
             
             ans = mp[(i, j)]
             # cut through width
-            for k in range(1, j):
+            for k in range(1, j//2+1):
                 ans = max(ans, dp(i, k)+dp(i, j-k))
             # cut through height
-            for k in range(1, i):
+            for k in range(1, i//2+1):
                 ans = max(ans, dp(k, j)+dp(i-k, j))
             return ans
         
