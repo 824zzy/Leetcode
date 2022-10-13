ca16202adc62a307f6a5836dd7e0490a27a10394
824zzy <zhengyuan.zhu@mavs.uta.edu>
2022-10-13 17:18:58 -0500

    daily challenge

---
+++ C_LinkList/DeleteNode/237_Delete_Node_in_a_Linked_List_L1.py
@@ -0,0 +1,10 @@
+""" https://leetcode.com/problems/delete-node-in-a-linked-list/
+dont think about previous node, just change the value of current node
+
+1. carry over the value of next node;
+2. delete next node.
+"""
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
