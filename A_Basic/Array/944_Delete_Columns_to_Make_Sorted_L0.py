8f806c95010aa7d6051d78d9d52dcd17904fe2c3
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-01-03 10:47:08 -0600

    daily challenge

--- A_Basic/Array/944_Delete_Columns_to_Make_Sorted_L0.py
+++ A_Basic/Array/944_Delete_Columns_to_Make_Sorted_L0.py
@@ -1,6 +1,8 @@
 """ https://leetcode.com/problems/delete-columns-to-make-sorted/
 use zip to find all the columns and check if each column is lexicographically
 """
+from header import *
+
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         cols = [list(s) for s in zip(*strs)]
---
+++ D_TwoPointers/SameDirection/271_Encode_and_Decode_Strings_L1.py
@@ -0,0 +1,49 @@
+""" https://leetcode.com/problems/encode-and-decode-strings/
+1. Chunked transfer encoding, form the string by length of each string and the string itself.
+2. use two pointers to decode the string
+"""
+from header import *
+
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        ans = ''
+        for s in strs:
+            ans += str(len(s))+'|'+s
+        return ans
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        ans = []
+        i = 0
+        j = 0
+        while j<len(s):
+            while s[j]!='|':
+                j += 1
+            l = int(s[i:j])
+            ans.append(s[j+1:j+l+1])
+            j += l+1
+            i = j
+        return ans
+    
+# cheating by special splitter
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        splitter = '!#$%^&*()'
+        return splitter.join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split('!#$%^&*()')
+        
+
+"""
+["63/Rc","h","BmI3FS~J9#vmk","7uBZ?7*/","24h+X","O "]
+"""
