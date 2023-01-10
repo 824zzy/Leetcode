b176c01f6af3e4944c640826ac20c7aefa49e418
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-01-10 11:54:47 -0600

    daily challenge

---
+++ G_BinarySearch/AdvancedBinarySearch/441_Arranging_Coins_L0.py
@@ -0,0 +1,15 @@
+""" https://leetcode.com/problems/arranging-coins/
+find complete rows using middle number through m*(m+1)//2
+"""
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        def fn(m):
+            if m*(m+1)//2>n: return True
+            else: return False
+            
+        l, r = 0, n+1
+        while l<r:
+            m = (l+r)//2
+            if fn(m): r = m
+            else: l = m + 1
+        return l-1
