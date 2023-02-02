9afb78248a92bda8c22df08d579fd67cfb05287d
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-02-01 18:41:42 -0600

    daily challenge

---
+++ B_HashTable/Basic/953_Verifying_an_Alien_Dictionary_L0.py
@@ -0,0 +1,11 @@
+""" https://leetcode.com/problems/verifying-an-alien-dictionary/
+1. use a map to convert alien to english
+2. check if the converted words are sorted
+"""
+from header import *
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        mp = {order[i]: chr(i+97) for i in range(26)}
+        A = [''.join(mp[c] for c in w) for w in words]
+        return A==sorted(A)
