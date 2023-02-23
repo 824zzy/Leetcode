a5602c016d3124e0221fafa21a5bf7b45325d2ee
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-02-23 11:43:55 -0600

    daily challenge

--- G_BinarySearch/AdvancedBinarySearch/1011_Capacity_To_Ship_Packages_Within_D_Days_L2.py
+++ G_BinarySearch/AdvancedBinarySearch/1011_Capacity_To_Ship_Packages_Within_D_Days_L2.py
@@ -3,18 +3,24 @@ the same as 410
 Use binary search to check if the array can be splitted as multiple subarray whose sum is m.
 Note that the lower bound is max(A)
 """
+from header import *
+
 class Solution:
-    def splitArray(self, A: List[int], n: int) -> int:
-        def fn(m, n):
-            _m = m
+    def splitArray(self, A: List[int], k: int) -> int:
+        def fn(least):
+            sm = 0
+            _k = 1
             for x in A:
-                if _m-x>=0: _m -= x
-                else: _m, n = m-x, n-1
-            return n>0
-            
+                if sm+x>least:
+                    sm = x
+                    _k += 1
+                else:
+                    sm += x
+            return _k<=k
+                    
         l, r = max(A), sum(A)
         while l<r:
             m = (l+r)//2
-            if fn(m, n): r = m
-            else: l = m + 1
+            if fn(m): r = m
+            else: l = m+1
         return l
---
+++ Z_Advanced/Sorting/MergeSort/2570_Merge_Two_2D_Array_by_Summing_Values_L1.py
@@ -0,0 +1,36 @@
+""" https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/
+merge sort
+"""
+from header import *
+
+class Solution:
+    def mergeArrays(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        ans = []
+        i, j = 0, 0
+        m, n = len(A), len(B)
+        while 1:
+            if i==m:
+                ans.extend(B[j:])
+                break
+            if j==n:
+                ans.extend(A[i:])
+                break
+            if A[i][0]<B[j][0]:
+                ans.append(A[i])
+                i += 1
+            elif A[i][0]>B[j][0]:
+                ans.append(B[j])
+                j += 1
+            else:
+                A[i][1] += B[j][1]
+                ans.append(A[i])
+                i += 1
+                j += 1
+        return ans
+        
+class Solution:
+    def mergeArrays(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        mp = defaultdict(int)
+        for k, v in A: mp[k] = v
+        for k, v in B: mp[k] += v
+        return sorted(mp.items(), key=lambda x: x[0])
