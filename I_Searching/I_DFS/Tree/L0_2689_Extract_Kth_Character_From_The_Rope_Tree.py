ee07850613b7f7512d15f194b3d44bb30185185c
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-05-19 02:03:05 -0500

    refactor

---
+++ I_Searching/I_DFS/Tree/L0_2689_Extract_Kth_Character_From_The_Rope_Tree.py
@@ -0,0 +1,35 @@
+""" https://leetcode.com/problems/extract-kth-character-from-the-rope-tree/
+inorder traversal
+"""
+from header import *
+
+class Solution:
+    def getKthCharacter(self, root: Optional[object], k: int) -> str:
+        def dfs(node, k):
+            if not node: return 0, ''
+            if not node.left and not node.right:
+                if k-1<len(node.val):
+                    return k, node.val[k-1]
+                else:
+                    return len(node.val), ''
+                
+            l, ans_l = dfs(node.left, k)
+            r, ans_r = dfs(node.right, k-l)
+            return l+r, ans_l or ans_r
+        
+        return dfs(root, k)[1]
+    
+    
+    
+class Solution:
+    def getKthCharacter(self, root: Optional[object], k: int) -> str:
+        def dfs(node):
+            if not node: return ''
+            if not node.left and not node.right:
+                return node.val
+                
+            l = dfs(node.left)
+            r = dfs(node.right)
+            return l+r
+        
+        return dfs(root)[k-1]
--- I_Searching/J_BFS/BipartiteGraph/L1_886_Possible_Bipartition.py
+++ I_Searching/J_BFS/BipartiteGraph/L1_886_Possible_Bipartition.py
@@ -6,6 +6,33 @@ classical bipartite problem:
 """
 from header import *
 
+# dfs solution
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        def dfs(i):
+            ans = True
+            for j in G[i]:
+                if seen[j]==0:
+                    seen[j] = -seen[i]
+                    ans &= dfs(j)
+                elif seen[j]==seen[i]:
+                    return False
+            return ans
+
+        G = defaultdict(list)
+        for i, j in dislikes:
+            G[i].append(j)
+            G[j].append(i)
+
+        seen = [0]*(n+1)
+        for i in range(1, n+1):
+            if seen[i]==0:
+                seen[i] = 1
+                if not dfs(i):
+                    return False
+        return True
+    
+# bfs solution
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         G = defaultdict(list)
