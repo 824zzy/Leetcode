4dbe1f73a135f1279c869614f80933c2e5eff58f
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-06-06 14:49:16 -0500

    daily challenge

--- A_Basic/Array/L0_1502_Can_Make_Arithmetic_Progression_From_Sequence.py
+++ A_Basic/Array/L0_1502_Can_Make_Arithmetic_Progression_From_Sequence.py
@@ -1,6 +1,8 @@
 """ https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
 sort the array
 """
+from header import *
+
 class Solution:
     def canMakeArithmeticProgression(self, A: List[int]) -> bool:
         if len(A)==2: return True
