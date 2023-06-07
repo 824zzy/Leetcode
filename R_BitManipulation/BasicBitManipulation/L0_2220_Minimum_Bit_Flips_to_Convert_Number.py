c3f895e03cb51f436d85bc9b906eb4ac1589b3a9
824zzy <zhengyuan.zhu@mavs.uta.edu>
2023-06-06 21:51:43 -0500

    daily challenge

---
+++ R_BitManipulation/BasicBitManipulation/L0_2220_Minimum_Bit_Flips_to_Convert_Number.py
@@ -0,0 +1,17 @@
+""" https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+Solution 1: XOR and count "1"
+Solution 2: compare bit by bit
+"""
+class Solution:
+    def minBitFlips(self, S: int, G: int) -> int:
+        return bin(S^G).count('1')
+
+
+class Solution:
+    def minBitFlips(self, s: int, g: int) -> int:
+        ans = 0
+        while s or g:
+            s, ss = divmod(s, 2)
+            g, gg = divmod(g, 2)
+            if ss!=gg: ans +=1
+        return ans
