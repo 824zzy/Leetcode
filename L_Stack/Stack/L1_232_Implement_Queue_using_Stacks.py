a7da89793b9d2565d3e17992283dcc8126fca678
824zzy <zhengyuan.zhu@mavs.uta.edu>
2024-01-29 17:44:05 -0600

    practice

--- L_Stack/Stack/L1_232_Implement_Queue_using_Stacks.py
+++ L_Stack/Stack/L1_232_Implement_Queue_using_Stacks.py
@@ -1,42 +1,31 @@
 """ https://leetcode.com/problems/implement-queue-using-stacks/
 using 2 stacks to implement a queue
 """
-# Using 2 stacks
 class MyQueue:
     def __init__(self):
-        self.stk1 = []
-        self.stk2 = []
+        self.A = []
+        self.B = []
 
     def push(self, x: int) -> None:
-        self.stk1.append(x)
+        self.A.append(x)
 
     def pop(self) -> int:
-        ans = self.peek()
-        self.stk2.pop()
-        return ans        
-
+        if self.B:
+            return self.B.pop()
+        else:
+            while self.A:
+                self.B.append(self.A.pop())
+            if self.B:
+                return self.B.pop()
+        
     def peek(self) -> int:
-        if not self.stk2:
-            while self.stk1:
-                self.stk2.append(self.stk1.pop())
-        return self.stk2[-1]
-
+        if self.B:
+            return self.B[-1]
+        else:
+            while self.A:
+                self.B.append(self.A.pop())
+            if self.B:
+                return self.B[-1]
+        
     def empty(self) -> bool:
-        return (len(self.stk1)+len(self.stk2))==0
-
-# cheating
-class MyQueue:
-    def __init__(self):
-        self.q = []
-
-    def push(self, x: int) -> None:
-        self.q.append(x)
-
-    def pop(self) -> int:
-        return self.q.pop(0)
-
-    def peek(self) -> int:
-        return self.q[0]
-
-    def empty(self) -> bool:
-        return len(self.q)==0
+        return (len(self.A)+len(self.B)) == 0
