e042c90608ca761c8d51597bd65ecbc979d08c9d
824zzy <zhengyuan.zhu@mavs.uta.edu>
2024-02-06 01:24:37 -0600

    dc

--- E_SlidingWindow/DynamicWindow/L1_76_Minimum_Window_Substring.py
+++ E_SlidingWindow/DynamicWindow/L1_76_Minimum_Window_Substring.py
@@ -1,19 +1,22 @@
 """ https://leetcode.com/problems/minimum-window-substring/
 1. use a dynamic sliding windows and a Counter to count t's frequency
-2. if all Counter's frequencies are less than 0, then increase i
+2. if all Counter's frequencies are more than t, then increase i
 """
 from header import *
 
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        cnt = Counter(t)
+    def minWindow(self, A: str, t: str) -> str:
+        t = Counter(t)
+        cnt = Counter()
         i = 0
-        ans = '*'*(len(s)+1)
-        for j in range(len(s)):
-            cnt[s[j]] -= 1
-            while all(v<=0 for k, v in cnt.items()):
-                ans = min(ans, s[i:j+1], key=len)
-                if s[i] in cnt:
-                    cnt[s[i]] += 1
+        ans = (0, len(A)+1)
+        
+        for j in range(len(A)):
+            cnt[A[j]] += 1
+            while cnt>=t:
+                ans = min(ans, (i, j), key=lambda x: (x[1]-x[0]))
+                cnt[A[i]] -= 1
+                if cnt[A[i]]==0:
+                    cnt.pop(A[i])
                 i += 1
-        return ans if ans != '*'*(len(s)+1) else ''
+        return A[ans[0]:ans[1]+1] if ans!=(0, len(A)+1) else ''
---
+++ K_DynamicProgramming/SequentialDP/L1_1043_Partition_Array_for_Maximum_Sum.py
@@ -0,0 +1,18 @@
+""" https://leetcode.com/problems/partition-array-for-maximum-sum/
+sequential dp with K-length window
+"""
+from header import *
+
+class Solution:
+    def maxSumAfterPartitioning(self, A: List[int], k: int) -> int:
+        @cache
+        def dp(i):
+            if i>=len(A):
+                return 0
+            mx = 0
+            ans = 0
+            for j in range(i, min(i+k, len(A))):
+                mx = max(mx, A[j])
+                ans = max(ans, (j-i+1)*mx+dp(j+1))
+            return ans
+        return dp(0)
