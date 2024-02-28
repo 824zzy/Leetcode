623127d0f6297b8216cbabd027ef0ede186e6102
824zzy <zhengyuan.zhu@mavs.uta.edu>
2024-02-28 11:25:03 -0600

    misc

--- I_Searching/BFS/Tree/L0_513_Find_Bottom_Left_Tree_Value.py
+++ I_Searching/BFS/Tree/L0_513_Find_Bottom_Left_Tree_Value.py
@@ -1,11 +1,33 @@
+""" https://leetcode.com/problems/find-bottom-left-tree-value/
+"""
+from header import *
+
+# bfs solution
 class Solution:
-    def findBottomLeftValue(self, root: TreeNode) -> int:
-        queue = [root]
-        ans = 0
-        while queue:
-            for i in range(len(queue)):
-                cur = queue.pop(0)
-                if i==0: ans = cur.val
-                if cur.left: queue.append(cur.left)
-                if cur.right: queue.append(cur.right)
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        Q = [root]
+        while Q:
+            nxtQ = []
+            for i, node in enumerate(Q):
+                if i==0:
+                    ans = node.val
+                if node.left:
+                    nxtQ.append(node.left)
+                if node.right:
+                    nxtQ.append(node.right)
+            Q = nxtQ
         return ans
+    
+    
+# dfs solution
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        self.ans = {}
+        def dfs(node, d):
+            if not node:
+                return
+            dfs(node.left, d+1)
+            self.ans.setdefault(d, node.val)
+            dfs(node.right, d+1)
+        dfs(root, 0)
+        return self.ans[max(self.ans)]
