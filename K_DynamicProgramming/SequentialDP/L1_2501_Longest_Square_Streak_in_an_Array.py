aecc8f21d36f659eb635b87c7b0c67501e9c2236
824zzy <zhengyuan.zhu@mavs.uta.edu>
2024-10-28 14:31:00 -0500

    dc

--- K_DynamicProgramming/SequentialDP/L1_2501_Longest_Square_Streak_in_an_Array.py
+++ K_DynamicProgramming/SequentialDP/L1_2501_Longest_Square_Streak_in_an_Array.py
@@ -1,6 +1,7 @@
 """ https://leetcode.com/problems/longest-square-streak-in-an-array/description/
 dp[x] = dp[isqrt(x)] + 1
 """
+
 from header import *
 
 
@@ -13,3 +14,16 @@ class Solution:
             if sqrt(x) == isqrt(x):
                 dp[x] = dp[isqrt(x)] + 1
         return max(dp.values()) if max(dp.values()) != 1 else -1
+
+
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        A = [0] * 100001
+        for n in nums:
+            A[n] = 1
+
+        for i in range(317):
+            if A[i] > 0 and A[i * i] > 0:
+                A[i * i] = A[i] + 1
+        mx = max(A)
+        return mx if mx != 1 else -1
